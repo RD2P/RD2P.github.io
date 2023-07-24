6e7f6a959cb8b11f4bb609f174366b49e93f2c5e
Raphael <graphael.dr@gmail.com>
2023-07-23 19:12:41 -0600

    Added main image

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-
-import './App.css'
+import egypt from './assets/egypt-art.jpg'
+import './styles/style.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +8,7 @@ function App() {
   return (
     <>
       <h1>Welcome to the Library</h1>
+      <img className='main-image' src={egypt}/>
 
     </>
   )
