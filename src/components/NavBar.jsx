697d11eb65b22b0d29a0736445836a3846b17c98
Raphael <graphael.dr@gmail.com>
2023-09-13 20:38:32 -0600

    Added navbar component with styles

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+import '../styles/navbar.css'
+
+const NavBar = () => {
+  return (
+    <header className="navbar">
+      <nav>
+        <a href="#/">Home</a>
+        <a href="#/">Work</a>
+      </nav>
+      <h3 className="name">Raphael De Los Reyes</h3>
+      <div className="socials-icons">
+        <a href="https://github.com/RD2P" className="social"> 
+         <i className="fa-brands fa-square-github fa-lg"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/glennraphaeldlr/" className="social"> 
+          <i className="fa-brands fa-linkedin  fa-lg"></i>
+        </a>
+      </div>
+    </header>
+  )
+};
+
+export default NavBar;
