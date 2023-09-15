f18babbef2f88b6049d899557b9ec9181cbef0a5
Raphael <graphael.dr@gmail.com>
2023-09-14 22:00:19 -0600

    Improved Hero layout

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,18 @@
+import "../styles/hero.css"
+import egyptArt from "../assets/egypt-art.jpg";
+import rafpic from '../assets/raf.jpg'
+
 const Hero = () => {
-  return <h1>Hero</h1>;
+  return (
+    <section className="hero">
+      <div className="prof-pic-wrapper">
+        <img src={rafpic} className="prof-pic"></img>
+      </div>
+      <div className="about-me-wrapper">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, voluptatem dolores quibusdam natus voluptate, et ut in itaque odio atque illo soluta repellendus quae? Et error nesciunt eaque. Corrupti, asperiores.</p>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
