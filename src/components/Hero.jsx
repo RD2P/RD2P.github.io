158ef380737fd2126426b42ee1e4692ad6ee7723
Raphael <graphael.dr@gmail.com>
2023-09-18 14:52:57 -0600

    Layout fix for Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
         <img src={rafpic} className="prof-pic"></img>
       </div>
       <div className="about-me-wrapper">
-        <p>Hi, I'm Raphael. I am an Engineering student at the University of Victoria. I have a strong passion for web development with experience in HTML, CSS, JavaScript, React, and React Native. I have completed several projects (check them out below!), and I'm still learning a little more each day. <br />Thank you for visiting!</p>
+        <p className="about-me-txt">Hi, I'm Raphael. I am an Engineering student at the University of Victoria. I have a strong passion for web development with experience in HTML, CSS, JavaScript, React, and React Native. I have completed several projects (check them out below!), and I'm still learning a little more each day. <br />Thank you for visiting!</p>
       </div>
     </section>
   );
