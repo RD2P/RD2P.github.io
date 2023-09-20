a9f1da7282c4eb8bda5c7d2aa11e92db79808ad0
Raphael <graphael.dr@gmail.com>
2023-09-20 13:31:43 -0600

    Removed non-working nav links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,17 +3,13 @@ import '../styles/navbar.css'
 const NavBar = () => {
   return (
     <header className="navbar">
-      <nav>
-        <a href="https://rd2p.github.io/">Home</a>
-        <a href="#/">Work</a>
-      </nav>
-      <h3 className="name">Raphael De Los Reyes</h3>
-      <div className="socials-icons">
+      <h3 className="navbar--title">Raphael De Los Reyes</h3>
+      <div className="navbar--icons">
         <a href="https://github.com/RD2P" className="social"> 
          <i className="fa-brands fa-square-github fa-lg"></i>
         </a>
         <a href="https://www.linkedin.com/in/glennraphaeldlr/" className="social"> 
-          <i className="fa-brands fa-linkedin  fa-lg"></i>
+          <i className="fa-brands fa-linkedin  fa-lg" ></i>
         </a>
       </div>
     </header>
