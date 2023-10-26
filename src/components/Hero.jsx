5131d47c14082806b15389c28588562595ae405d
Raphael <graphael.dr@gmail.com>
2023-10-25 20:38:37 -0600

    updated hero layout

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,18 +3,20 @@ import rafpic from '../assets/raf.jpg'
 
 const Hero = () => {
   return (
-    <div className="container">
       <section className="hero">
-        <div className="hero__image">
-          <img src={rafpic} className="prof-pic"></img>
-        </div>
-        <div>
-          <div className="hero__text">
-            <p className="about-me-txt">Hi, I'm Raphael.<br/>I like tinkering and building things with code. Check out some of my work below!</p>
+        <div className="container">
+          <div className="hero__wrapper">
+            <div className="hero__image">
+              <img src={rafpic} className="prof-pic"></img>
+            </div>
+            <div>
+              <div className="hero__text">
+                <p className="about-me-txt">Hi, I'm Raphael.<br/>I like learning and building things with code. Check out some of my work below!</p>
+              </div>
+          </div>
           </div>
         </div>
       </section>
-    </div>
   );
 };
 
