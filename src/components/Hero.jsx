1a3c3827eb5f7299cee30f8ac0bf625c18cf762f
Raphael <graphael.dr@gmail.com>
2023-10-25 20:58:26 -0600

    update hero responsiveness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
             <div className="hero__image">
               <img src={rafpic} className="prof-pic"></img>
             </div>
-            <div>
+            <div className="hero__text__wrapper">
               <div className="hero__text">
                 <p className="about-me-txt">Hi, I'm Raphael.<br/>I like learning and building things with code. Check out some of my work below!</p>
               </div>
