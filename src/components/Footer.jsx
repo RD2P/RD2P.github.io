6895ac8a55954252dea04223e186566fd3414034
Raphael <graphael.dr@gmail.com>
2023-10-25 20:51:11 -0600

    Updated layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,14 @@ import '../styles/footer.css'
 
 const Footer = () => {
   return(
-    <div className="container">
-      <footer className="footer">
-        <h3>Raphael De Los Reyes</h3>
-        <p>graphael.dr@gmail.com</p>
-      </footer>
-    </div>
+    <footer className="footer">
+      <div className="container">
+        <div className="footer__wrapper">
+          <h3>Raphael De Los Reyes</h3>
+          <p>graphael.dr@gmail.com</p>
+        </div>
+      </div>
+    </footer>
   )
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,14 +3,18 @@ import '../styles/navbar.css'
 const NavBar = () => {
   return (
       <nav className="navbar">
-        <h3 className="navbar__title">Raphael De Los Reyes</h3>
-        <div className="navbar__icons">
-          <a href="https://github.com/RD2P" className="navbar__icon">
-           <i className="fa-brands fa-square-github fa-lg"></i>
-          </a>
-          <a href="https://www.linkedin.com/in/glennraphaeldlr/" className="navbar__icon">
-            <i className="fa-brands fa-linkedin  fa-lg" ></i>
-          </a>
+        <div className="container">
+          <div className="navbar__wrapper">
+            <h3 className="navbar__title">Raphael De Los Reyes</h3>
+            <div className="navbar__icons">
+              <a href="https://github.com/RD2P" className="navbar__icon">
+               <i className="fa-brands fa-square-github fa-lg"></i>
+              </a>
+              <a href="https://www.linkedin.com/in/glennraphaeldlr/" className="navbar__icon">
+                <i className="fa-brands fa-linkedin  fa-lg" ></i>
+              </a>
+            </div>
+          </div>
         </div>
       </nav>
   )
