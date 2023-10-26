201101675cd5ad63222650a79fe6ec2991dcbde8
Raphael <graphael.dr@gmail.com>
2023-10-25 20:53:30 -0600

    removed old note import from app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import NavBar from "./components/NavBar";
 import Hero from './components/Hero.jsx'
 import Projects from './components/Projects.jsx'
-import Note from './components/Note.jsx'
 import Footer from './components/Footer'
 
 import "./styles/main.css";
