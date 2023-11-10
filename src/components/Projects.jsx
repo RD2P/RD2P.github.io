274283d8be9ae6d366323b0d4ebf4f4155dc4613
Raphael <graphael.dr@gmail.com>
2023-11-10 11:27:53 -0600

    Update projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,78 +6,78 @@ import hexnote from "../assets/hexnote.png"
 
 const Projects = () => {
 
-  return(
+  return (
     <>
       <div className="container">
         <h2 className="projects__title">Projects</h2>
       </div>
       <div className="container">
-          <div className="project__rows">
+        <div className="project__rows">
           <section className="project__row">
-              <div className="card--wrapper card--wide">
-                <Card
-                  link=""
-                  image={hexnote}
-                  title="Hex Note"
-                />
+            <div className="card--wrapper card--wide">
+              <Card
+                link=""
+                image={hexnote}
+                title="Hex Note"
+              />
+            </div>
+            <div className="project-description">
+              <div className="description-text">
+                <p>A full-stack web application that allows you to create hex-encoded notes and save them in your computer.</p>
+                <p className="tech-used">Node Express React</p>
               </div>
-              <div className="project-description">
-                <div className="description-text">
-                  <p>A full-stack web application that allows you to create hex-encoded notes and save them in your computer.</p>
-                  <p className="tech-used">Node Express React</p>
-                </div>
-                <div className="card__buttons">
-                  <a className="card__button disabled-button tooltip" // href="#"
+              <div className="card__buttons">
+                <a className="card__button disabled-button tooltip" // href="#"
                   target="_blank" >
-                    <span className="tooltiptext">In progress 🙂</span>
-                    DEMO
-                  </a>
-                  <a className="card__button" href="https://github.com/RD2P/HexNote" target="_blank">
-                    CODE
-                  </a>
-                </div>
+                  <span className="tooltiptext">In progress 🙂</span>
+                  DEMO
+                </a>
+                <a className="card__button" href="https://github.com/RD2P/HexNote" target="_blank">
+                  CODE
+                </a>
               </div>
-            </section>
-            <section className="project__row">
-              <div className="card--wrapper card--wide">
-                <Card
-                  link="https://rd2p.github.io/vaultkeeper"
-                  image={vaultkeeper}
-                  title="Vault Keeper"
-                  />
+            </div>
+          </section>
+          <section className="project__row">
+            <div className="card--wrapper card--wide">
+              <Card
+                link="https://rd2p.github.io/vaultkeeper"
+                image={vaultkeeper}
+                title="Vault Keeper"
+              />
+            </div>
+            <div className="project-description">
+              <div className="description-text">
+                <p>A password manager which uses a custom cipher.</p>
+                <p className="tech-used">React</p>
               </div>
-              <div className="project-description">
-                <div className="description-text">
-                  <p>A password manager which uses a custom cipher.</p>
-                  <p className="tech-used">React</p>
-                </div>
-                <div className="card__buttons">
-                  <a className="card__button" href="https://rd2p.github.io/vaultkeeper/" target="_blank">DEMO</a>
-                  <a className="card__button" href="https://github.com/RD2P/vaultkeeper" target="_blank">CODE</a>
-                </div>
+              <div className="card__buttons">
+                <a className="card__button" href="https://rd2p.github.io/vaultkeeper/" target="_blank">DEMO</a>
+                <a className="card__button" href="https://github.com/RD2P/vaultkeeper" target="_blank">CODE</a>
               </div>
-            </section>
-            <section className="project__row">
-              <div className="card--wrapper card--narrow">
-                <Card
-                  link="https://rd2p.github.io/samsung-calculator/"
-                  image={calculator}
-                  title="Calculator"
-                />
+            </div>
+          </section>
+          <section className="project__row">
+            <div className="card--wrapper card--narrow">
+              <Card
+                link="https://rd2p.github.io/samsung-calculator/"
+                image={calculator}
+                title="Calculator"
+              />
+            </div>
+            <div className="project-description">
+              <div className="description-text">
+                <p>An implementation of Samsung's calculator app.</p>
+                <p className="tech-used">HTML CSS JavaScript</p>
               </div>
-              <div className="project-description">
-                <div className="description-text">
-                  <p>An implementation of Samsung's calculator app.</p>
-                  <p className="tech-used">HTML CSS JavaScript</p>
-                </div>
-                <div className="card__buttons">
-                  <a className="card__button" href="https://rd2p.github.io/samsung-calculator/" target="_blank">DEMO</a>
-                  <a className="card__button" href="https://github.com/RD2P/samsung-calculator" target="_blank">CODE</a>
-                </div>
+              <div className="card__buttons">
+                <a className="card__button" href="https://rd2p.github.io/samsung-calculator/" target="_blank">DEMO</a>
+                <a className="card__button" href="https://github.com/RD2P/samsung-calculator" target="_blank">CODE</a>
               </div>
-            </section>
-          </div>
-        
+            </div>
+          </section>
+        </div>
+
       </div>
     </>
   )
