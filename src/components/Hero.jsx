c2c944caa2e931ba94f041bcc240a916c47ace77
Raphael <graphael.dr@gmail.com>
2023-11-10 11:03:35 -0600

    Update hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,20 +3,20 @@ import rafpic from '../assets/raf.jpg'
 
 const Hero = () => {
   return (
-      <section className="hero">
-        <div className="container">
-          <div className="hero__wrapper">
-            <div className="hero__image">
-              <img src={rafpic} className="prof-pic"></img>
-            </div>
-            <div className="hero__text__wrapper">
-              <div className="hero__text">
-                <p className="about-me-txt">Hi, I'm Raphael.<br/>I like learning and building things with code. Check out some of my work below!</p>
-              </div>
+    <section className="hero">
+      <div className="container">
+        <div className="hero__wrapper">
+          <div className="hero__image">
+            <img src={rafpic} className="prof-pic"></img>
           </div>
+          <div className="hero__text__wrapper">
+            <div className="hero__text">
+              <p className="about-me-txt">Hi, I'm Raphael.<br />I like learning and building things with code. Check out some of my work below!</p>
+            </div>
           </div>
         </div>
-      </section>
+      </div>
+    </section>
   );
 };
 
