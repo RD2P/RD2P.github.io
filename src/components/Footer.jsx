8bdd768d8a571d9ecaa0b01fa2c49cdb8cfc2207
Raphael <graphael.dr@gmail.com>
2023-11-10 11:32:24 -0600

    Update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,11 @@
 import '../styles/footer.css'
 
 const Footer = () => {
-  return(
+  return (
     <footer className="footer">
       <div className="container">
         <div className="footer__wrapper">
-          <h3>Raphael De Los Reyes</h3>
-          <p>graphael.dr@gmail.com</p>
+          <p>@{new Date().getFullYear()} Raphael De Los Reyes</p>
         </div>
       </div>
     </footer>
