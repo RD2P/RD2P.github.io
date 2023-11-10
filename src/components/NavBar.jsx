9d1e0ffe08cd8e136282836e40dd4a46dcd7e0cc
Raphael <graphael.dr@gmail.com>
2023-11-10 10:56:19 -0600

    Update navbar colors

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,28 +2,31 @@ import '../styles/navbar.css'
 
 const NavBar = () => {
   return (
-      <nav className="navbar">
-        <div className="container">
-          <div className="navbar__wrapper">
-            <h2 className="navbar__title">Raphael De Los Reyes</h2>
-            <div className="navbar__icons">
-              <a
-                href="https://github.com/RD2P"
-                target="_blank" 
-                className="navbar__icon">
-                <i className="devicon-github-original"></i>
-              </a>
-              
-              <a
-                href="https://www.linkedin.com/in/glennraphaeldlr/"
-                target="_blank"
-                className="navbar__icon">
-                <i className="devicon-linkedin-plain"></i>
-              </a>
-            </div>
+    <nav className="navbar">
+      <div className="container">
+        <div className="navbar__wrapper">
+          <h2 className="navbar__title">
+            Raphael De Los Reyes
+          </h2>
+          {/* icons */}
+          <div className="navbar__icons">
+            <a
+              href="https://github.com/RD2P"
+              target="_blank"
+              className="navbar__icon">
+              <i className="devicon-github-original"></i>
+            </a>
+
+            <a
+              href="https://www.linkedin.com/in/glennraphaeldlr/"
+              target="_blank"
+              className="navbar__icon">
+              <i className="devicon-linkedin-plain"></i>
+            </a>
           </div>
         </div>
-      </nav>
+      </div>
+    </nav>
   )
 };
 
