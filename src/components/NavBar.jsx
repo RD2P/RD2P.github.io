c304d5d03acc7db45fb88f253a3b43175395f459
Raphael <graphael.dr@gmail.com>
2024-02-01 21:29:48 -0600

    Add router navigation links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,31 @@
 import '../styles/navbar.css'
+import { Link } from "react-router-dom"
 
 const NavBar = () => {
   return (
     <nav className="navbar">
       <div className="container">
         <div className="navbar__wrapper">
+
           <h2 className="navbar__title">
             Raphael De Los Reyes
           </h2>
+
           {/* links */}
+          <div>
+            <Link
+              to="/"
+              className="navbar__link ">
+              Home
+            </Link>
+            <Link
+              to="/resume"
+              className="navbar__link ">
+              Resume
+            </Link>
+          </div>
+
+          {/* Icons */}
           <div className="navbar__icons">
             <a
               href="https://blog.raphaeldelosreyes.com/"
@@ -22,7 +39,6 @@ const NavBar = () => {
               className="navbar__link">
               GitHub
             </a>
-
             <a
               href="https://www.linkedin.com/in/glennraphaeldlr/"
               target="_blank"
