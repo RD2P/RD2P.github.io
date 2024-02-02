72b480714fd8d4c21c0a8f50e5f55880b831627e
Raphael <graphael.dr@gmail.com>
2024-02-01 21:10:58 -0600

    place hero, skills, and projects components into home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from './Hero.jsx'
+import Skills from './Skills'
+import Projects from './Projects.jsx'
+
+const Home = () => {
+  return (
+    <main>
+      <Hero />
+      <Skills />
+      <Projects />
+    </main>
+  )
+}
+
+export default Home
