ed894b25b079e255757f902a74a85b568b6b0db9
Raphael <graphael.dr@gmail.com>
2024-03-04 22:55:50 -0600

    moved to pages folder

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Hero from '../components/Hero.jsx'
+import Skills from '../components/Skills.jsx'
+import Projects from '../components/Projects.jsx'
+import Contact from '../components/Contact.jsx'
+
+const Home = () => {
+  return (
+    <main>
+      <Hero />
+      <Skills />
+      <Projects />
+      <Contact />
+    </main>
+  )
+}
+
+export default Home
